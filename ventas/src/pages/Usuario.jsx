2d9a47ee49647d55c7fcd92a7d32f60e56ac0683
Jhonatan Pérez <yhperez22@gmail.com>
2021-11-11 07:42:14 -0500

    inicio

--- ventas/src/pages/Usuario.jsx
+++ ventas/src/pages/Usuario.jsx
@@ -1,11 +1,300 @@
-import React from 'react'
+import Header from "../components/header";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  ModalFooter,
+} from "reactstrap";
 
-const Usuario = () => {
+const data = [
+  { id: 1, nombre: "Juliana", rol: "Administrador",estado:"Autorizado"},
+  { id: 2, nombre: "Luisa", rol: "Vendedor",estado:"Autorizado"},
+  { id: 3, nombre: "Camila", rol: "Vendedor",estado:"Autorizado"},
+  { id: 4, nombre: "Luis", rol: "Administrador",estado:"Autorizado"},
+  { id: 5, nombre: "Sofia", rol: "Vendedor",estado:"Pendiente"},
+  { id: 6, nombre: "Andrea", rol: "Vendedor",estado:"No autorizado"}
+];
+
+class Usuario extends React.Component {
+  state = {
+    data: data,
+    modalActualizar: false,
+    modalInsertar: false,
+    form: {
+      id: "",
+      nombre: "",
+      rol: "",
+      estado:"",
+
+    },
+  };
+
+  mostrarModalActualizar = (dato) => {
+    this.setState({
+      form: dato,
+      modalActualizar: true,
+    });
+  };
+
+  cerrarModalActualizar = () => {
+    this.setState({ modalActualizar: false });
+  };
+
+  mostrarModalInsertar = () => {
+    this.setState({
+      modalInsertar: true,
+    });
+  };
+
+  cerrarModalInsertar = () => {
+    this.setState({ modalInsertar: false });
+  };
+
+  editar = (dato) => {
+    var contador = 0;
+    var arreglo = this.state.data;
+    arreglo.map((registro) => {
+      if (dato.id == registro.id) {
+        arreglo[contador].nombre = dato.nombre;
+        arreglo[contador].rol = dato.rol;
+        arreglo[contador].estado = dato.estado;
+
+      }
+      contador++;
+    });
+    this.setState({ data: arreglo, modalActualizar: false });
+  };
+
+  eliminar = (dato) => {
+    var opcion = window.confirm("Estás Seguro que deseas Eliminar el elemento "+dato.id);
+    if (opcion == true) {
+      var contador = 0;
+      var arreglo = this.state.data;
+      arreglo.map((registro) => {
+        if (dato.id == registro.id) {
+          arreglo.splice(contador, 1);
+        }
+        contador++;
+      });
+      this.setState({ data: arreglo, modalActualizar: false });
+    }
+  };
+
+  insertar= ()=>{
+    var valorNuevo= {...this.state.form};
+    valorNuevo.id=this.state.data.length+1;
+    var lista= this.state.data;
+    lista.push(valorNuevo);
+    this.setState({ modalInsertar: false, data: lista });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  render() {
+    
     return (
-        <div>
-            Aca es de Usuario
-        </div>
-    )
-}
+      <>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
+      <Header />
+        <Container>
+          <input class="bu1" placeholder="Buscar"/>
+        <br />
+          <Button color="success" onClick={()=>this.mostrarModalInsertar()}>Insertar Nuevo Usuario</Button>
+          <br />
+          <br />
+          <Table>
+            <thead class="tu ">
+              <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Rol</th>
+                <th>Estado</th>
+                <th>Accion</th>
+              </tr>
+            </thead>
 
-export default Usuario
+            <tbody class="tb1">
+              {this.state.data.map((dato) => (
+                <tr key={dato.id}>
+                  <td>{dato.id}</td>
+                  <td>{dato.nombre}</td>
+                  <td>{dato.rol}</td>
+                  <td>{dato.estado}</td>
+                  <td>
+                    <Button color="primary"
+                      onClick={() => this.mostrarModalActualizar(dato)}>Actualizar</Button>{" "}
+                    <Button color="danger" onClick={()=> this.eliminar(dato)}>Eliminar</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
+
+        <Modal isOpen={this.state.modalActualizar}>
+          <ModalHeader>
+           <div><h3>Editar Registro</h3></div>
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>
+               Id:
+              </label>
+            
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.form.id}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Nombre: 
+              </label>
+              <input
+                className="form-control"
+                name="nombre"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.nombre}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Rol: 
+              </label>
+              <input
+                className="form-control"
+                name="rol"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.rol}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Estado: 
+              </label>
+              <input
+                className="form-control"
+                name="estado"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.estado}
+              />
+            </FormGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.editar(this.state.form)}
+            >
+              Actualizar
+            </Button>
+            <Button
+              color="danger"
+              onClick={() => this.cerrarModalActualizar()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
+
+
+        <Modal isOpen={this.state.modalInsertar}>
+          <ModalHeader>
+           <div><h3>Insertar Nuevo Usuario</h3></div>
+          </ModalHeader>
+          <ModalBody>
+            <FormGroup>
+              <label>
+               Id:
+              </label>
+            
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.form.id}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Nombre: 
+              </label>
+              <input
+                className="form-control"
+                name="nombre"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.nombre}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Rol: 
+              </label>
+              <input
+                className="form-control"
+                name="rol"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.rol}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Estado: 
+              </label>
+              <input
+                className="form-control"
+                name="estado"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.estado}
+              />
+            </FormGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.insertar()}
+            >
+              Insertar
+            </Button>
+            <Button
+              className="btn btn-danger"
+              onClick={() => this.cerrarModalInsertar()}
+            >
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </>
+    );
+  }
+}
+export default Usuario;
